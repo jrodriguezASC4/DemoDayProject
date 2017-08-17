68cab568721b5156aaa6e313451f21ffe7c7522a
jrodriguezASC4 <joseph.rodriguez416@gmail.com>
2017-08-17 09:58:29 -0400

    first push

---
+++ demoday/data/material-design-box-shadows/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/sdthornton/pen/wBZdXq.
+
+ Based on https://medium.com/@Florian/freebie-google-material-design-shadow-helper-2a0501295a2d
---
+++ moneyTransfer/script.js
@@ -0,0 +1,57 @@
+jQuery(document).ready($ => {
+  $(document).ready(() => {
+    // let pin = (+!![] + []) + (!+[] + !![] + []) + (!+[] + !![] + !![] + []) + (!+[] + !![] + !![] + !![] + []);
+
+    let pin = 1234;
+
+    // let pin = document.getElementById("userInput").value;
+
+    let enterCode = "";
+    enterCode.toString();
+    
+    
+
+    $("#numbers button").click(function() {
+
+      let clickedNumber = $(this).text().toString();
+      enterCode += clickedNumber;
+      let lengthCode = parseInt(enterCode.length);
+      lengthCode--;
+      $(`#fields .numberfield:eq(${lengthCode})`).addClass("active");
+
+      if (lengthCode == 3) {
+
+        // Check the PIN
+        if (enterCode == pin) {
+          // Right PIN!
+          $("#fields .numberfield").addClass("right");
+          $("#numbers").addClass("hide");
+          $("#breadsticks p").html("<b>Good eets!</b><br>Restart at the top, fam");
+
+        } else {
+          // Wrong PIN!
+          $("#fields").addClass("miss");
+          enterCode = "";
+          setTimeout(() => {
+            $("#fields .numberfield").removeClass("active");
+          }, 200);
+          setTimeout(() => {
+            $("#fields").removeClass("miss");
+          }, 500);
+
+        }
+
+      } else {}
+
+    });
+    
+    $("#restartbtn").click(() => {
+      enterCode = "";
+      $("#fields .numberfield").removeClass("active");
+      $("#fields .numberfield").removeClass("right");
+      $("#numbers").removeClass("hide");
+      $("#breadsticks p").html("<strong>Confirm your PIN</strong><br>");
+    });
+
+  });
+});
